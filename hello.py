f6b92800b453482dfc6ee67371f5beedc3ee8124
Fernando <fernandodiasaraujo3@gmail.com>
2026-08-17 13:24:04 -0300

    Adiciona rota /status para verificar status da API

--- hello.py
+++ hello.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def hello_world():
     return "Hello World"
 
+@app.route("/status")
+def status():
+    return {"status": "online"}
+
 if __name__ == "__main__":
     app.run(debug=True)
